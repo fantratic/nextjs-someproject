90710aab4837553c158f11f22d0535f116e53fed
WorldHatesD <ferfrink3577@gmail.com>
2023-11-21 09:16:20 -0600

    Trying first commit..vscode doubting me again

---
+++ src/components/land.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const land = () => {
+  return (
+    <div
+    className="flex flex-col justify-center items-center h-screen w-screen bg-gray-900"
+    >
+      <p className="h4">Landing</p>
+      
+    </div>
+  )
+}
+
+export default land

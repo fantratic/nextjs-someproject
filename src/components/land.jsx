3f4a3aaa57809c6b5f17a0db8e43f639d540aed0
Deonte Palmer <ferfrink3577@gmail.com>
2023-11-26 20:49:58 -0600

    -cleanup

--- src/components/land.jsx
+++ src/components/land.jsx
@@ -27,10 +27,14 @@ const Land = ({ fixed }) => {
     }, []);
   return (
     <div
-    className=""
+    className=" mx-auto px-4 py-4 h-[25vh] bg-slate-900"
     >
       <h1 className="text-3xl font-bold titleName " styles={styles.titleName}>{fixed}<span ref={typer} className="bg-gradient-to-r from-indigo-400 to-cyan-400 bg-clip-text text-transparent
       "></span></h1>
+      
+      
+      
+
       
     </div>
   )

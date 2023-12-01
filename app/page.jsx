6c4d5609678b8ab5bdde154d8160325f2b9e8382
Deonte Palmer <ferfrink3577@gmail.com>
2023-12-01 14:16:13 -0600

    push anyway, gonna finihs later, navbar created.

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,18 @@
 import Land from '@/src/components/land'
 import Image from 'next/image'
+import Navbar from "@/src/components/navbar";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col p-24 w-full">
-      <Land 
-      fixed="Hello! I am, "
-      />
-    </main>
+      <>
+          <Navbar />
+        <main className="flex min-h-screen flex-col p-24 w-full">
+
+          <Land
+          fixed="Hello! I am, "
+          />
+        </main>
+    </>
   )
 }
+
